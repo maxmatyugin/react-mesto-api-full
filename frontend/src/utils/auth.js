1d23db39e862f2698ace59cda8d4b771cc8cfec6
Max Matyugin <sairentohiru@gmail.com>
2021-03-29 22:57:54 +0300

    process

--- frontend/src/utils/auth.js
+++ frontend/src/utils/auth.js
@@ -1,4 +1,4 @@
-export const BASE_URL = "https://api.front.maxmatyugin.nomoredomains.club/";
+export const BASE_URL = "https://api.front.maxmatyugin.nomoredomains.club";
 
 const handleRes = (res) =>
   res.ok ? res.json() : Promise.reject(`Ошибка ${res.status}`);
