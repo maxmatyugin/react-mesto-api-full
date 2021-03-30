5eda5f1b7ce2c69c66c580ac2caa69db065b5598
Max Matyugin <sairentohiru@gmail.com>
2021-03-30 13:01:24 +0300

    remove cors

--- backend/app.js
+++ backend/app.js
@@ -8,24 +8,24 @@ const routes = require('./routes/index.js');
 
 const app = express();
 
-const allowedCors = [
-  'https://api.front.maxmatyugin.nomoredomains.club',
-  'https://front.maxmatyugin.nomoredomains.club',
-  'http://front.maxmatyugin.nomoredomains.club',
-  'http://localhost:3001',
-  'http://localhost:3000',
-];
+// const allowedCors = [
+//   'https://api.front.maxmatyugin.nomoredomains.club',
+//   'https://front.maxmatyugin.nomoredomains.club',
+//   'http://front.maxmatyugin.nomoredomains.club',
+//   'http://localhost:3001',
+//   'http://localhost:3000',
+// ];
 
-const corsOptions = {
-  origin: allowedCors,
-  optionsSuccessStatus: 204,
-  methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
-  preflightContinue: false,
-  allowedHeaders: ['Content-Type', 'origin', 'Authorization'],
-  credentials: true,
-};
+// const corsOptions = {
+//   origin: allowedCors,
+//   optionsSuccessStatus: 204,
+//   methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
+//   preflightContinue: false,
+//   allowedHeaders: ['Content-Type', 'origin', 'Authorization'],
+//   credentials: true,
+// };
 
-app.use('*', cors(corsOptions));
+// app.use('*', cors(corsOptions));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
